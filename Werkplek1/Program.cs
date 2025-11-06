a2c6ab0f765687da2b84a340aaf8ec591b96dc8b
ahmed alhennawi <ahmedalhennawi@gmail.com>
2025-11-06 13:22:31 +0100

    demo-01

---
+++ Werkplek1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Werkplek1;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Gebruikersnaam: ");
+        string inputUser = Console.ReadLine();
+
+
+    }
+}
+
