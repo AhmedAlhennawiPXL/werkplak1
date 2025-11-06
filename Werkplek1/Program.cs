47a08000bc331e583d5fb2e81c5db7cec2b5e52e
ahmed alhennawi <ahmedalhennawi@gmail.com>
2025-11-06 13:33:24 +0100

    demo-04

--- Werkplek1/Program.cs
+++ Werkplek1/Program.cs
@@ -8,6 +8,7 @@ class Program
     {
         const string user = "Admin";
         const string password = "P@sswoord123";
+        int tries = 0;
 
         Console.Write("Gebruikersnaam: ");
         string inputUser = Console.ReadLine();
@@ -17,6 +18,8 @@ class Program
         if (inputUser == user && inputPassword == password)
             tries++;
         Console.WriteLine($"Foutieve login.\n{3 - tries} pogingen over.");
+        Console.WriteLine("Te veel pogingen. Toegang geweigerd.");
+
 
 
 
